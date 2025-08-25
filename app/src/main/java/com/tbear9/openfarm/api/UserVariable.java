d930b91378e8b8fc2f1ad1f769a49276ca6db935
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-08-25 22:07:17 +0700

    merged from plants-api

--- app/src/main/java/com/tbear9/openfarm/api/UserVariable.java
+++ app/src/main/java/com/tbear9/openfarm/api/UserVariable.java
@@ -2,7 +2,6 @@ package com.tbear9.openfarm.api;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.Singular;
 
 import java.io.Serial;
@@ -10,7 +9,6 @@ import java.io.Serializable;
 import java.util.Set;
 
 @Getter
-@Setter
 @Builder
 public class UserVariable implements Serializable {
     @Serial private static final long serialVersionUID = 1L;
