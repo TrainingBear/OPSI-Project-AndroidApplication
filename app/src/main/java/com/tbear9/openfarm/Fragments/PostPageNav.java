c77f998d8e62de7e60b69675db7a31e4d31d0c29
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-08-31 05:53:37 +0700

    post

--- app/src/main/java/com/tbear9/openfarm/Fragments/PostPageNav.java
+++ app/src/main/java/com/tbear9/openfarm/Fragments/PostPageNav.java
@@ -1,6 +1,7 @@
 package com.tbear9.openfarm.Fragments;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.tbear9.openfarm.R;
+import com.tbear9.openfarm.activities.PostActivity;
 import com.tbear9.openfarm.databinding.FragmentPostPageNavBinding;
 
 import lombok.Setter;
@@ -38,19 +40,27 @@ public class PostPageNav extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = FragmentPostPageNavBinding.inflate(getLayoutInflater());
-        binding.back.setOnClickListener( e -> listener.back());
-        binding.next.setOnClickListener( e -> listener.next());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return FragmentPostPageNavBinding.inflate(getLayoutInflater(), container, true).getRoot();
+        binding = FragmentPostPageNavBinding.inflate(getLayoutInflater(), container, false);
+        binding.back.setOnClickListener( e -> listener.back());
+        binding.next.setOnClickListener( e -> listener.next());
+        return binding.getRoot();
     }
 
     public interface Listener {
-        default void next(){};
-        default void back(){};
+        default void next(){
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                PostActivity.next();
+//            }
+        };
+        default void back(){
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//                PostActivity.back();
+//            }
+        };
     }
 }
