5bd2d142a613ad9e5499b37c6db8b8a217fe459e
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-09-04 20:53:43 +0700

    a

---
+++ app/src/main/java/com/tbear9/openfarm/BotakuhPengetahuanActivity.kt
@@ -0,0 +1,26 @@
+package com.TBear9.openfarm.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.TBear9.openfarm.MainActivity
+import com.tbear9.openfarm.databinding.BotakuhpengetahuanBinding
+
+class BotakuhPengetahuanActivity : AppCompatActivity() {
+
+    private lateinit var binding: BotakuhpengetahuanBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = BotakuhpengetahuanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aksi ketika tombol panah diklik
+        binding.tombolKembali.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Opsional: agar tidak bisa balik lagi pakai tombol back
+        }
+    }
+}
