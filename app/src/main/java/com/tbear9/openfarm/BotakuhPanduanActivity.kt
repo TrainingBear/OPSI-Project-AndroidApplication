c5a9874ae69b06920f0e6018ab43e0b49cca85f4
TrainingBear <77871999+TrainingBear@users.noreply.github.com>
2025-09-07 14:48:02 +0700

    api removal fixed a duplication package

--- app/src/main/java/com/tbear9/openfarm/BotakuhPanduanActivity.kt
+++ app/src/main/java/com/tbear9/openfarm/BotakuhPanduanActivity.kt
@@ -3,7 +3,7 @@ package com.TBear9.openfarm.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.TBear9.openfarm.MainActivity
+import com.tbear9.openfarm.MainActivity
 import com.tbear9.openfarm.databinding.BotakuhpanduanBinding
 
 class BotakuhPanduanActivity : AppCompatActivity() {
